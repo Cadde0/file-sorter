9c3cf4948c15e31c838f3a560acda31efd26508c
Cadde0 <ocaddeo@kth.se>
2024-11-26 17:38:21 +0100

    Sorting for Downloads and Image folders

--- sort_files.py
+++ sort_files.py
@@ -1,31 +1,43 @@
 import os, shutil
 
 # Define the directory containing the files
-path = r"/Users/oscarcaddeo/Documents/"
+documents_path = r"/Users/oscarcaddeo/Documents/"
 image_path = r"/Users/oscarcaddeo/Pictures/"
+downloads_path = r"/Users/oscarcaddeo/Downloads/"
 
-file_name = os.listdir(path)
+
+file_name_documents = os.listdir(documents_path)
 file_name_images = os.listdir(image_path)
+file_name_downloads = os.listdir(downloads_path)
 
 # Define folder names based on file types
 folder_names = ['pdf files', 'text files', 'csv files']
 
 # Create folders if they don't exist
 for loop in range(0,3):
-    if not os.path.exists(path + folder_names[loop]):
-        os.makedirs((path + folder_names[loop]))
+    if not os.path.exists(documents_path + folder_names[loop]):
+        os.makedirs((documents_path + folder_names[loop]))
 
 # Move files into their respective folders
-for file in file_name:
-    if ".pdf" in file and not os.path.exists(path + "pdf files/" + file):
-        shutil.move(path + file, path + "pdf files/" + file)
+for file in file_name_documents:
+    if ".pdf" in file and not os.path.exists(documents_path + "pdf files/" + file):
+        shutil.move(documents_path + file, documents_path + "pdf files/" + file)
     elif ".png" in file or ".jpeg" in file or ".jpg" in file:
-        shutil.move(path + file, image_path + file)
-    elif ".txt" in file and not os.path.exists(path + "text files/" + file):
-        shutil.move(path + file, path + "text files/" + file)
-    elif ".csv" in file and not os.path.exists(path + "csv files/" + file):
-        shutil.move(path + file, path + "csv files/" + file)
+        shutil.move(documents_path + file, image_path + file)
+    elif ".txt" in file and not os.path.exists(documents_path + "text files/" + file):
+        shutil.move(documents_path + file, documents_path + "text files/" + file)
+    elif ".csv" in file and not os.path.exists(documents_path + "csv files/" + file):
+        shutil.move(documents_path + file, documents_path + "csv files/" + file)
 
 #TODO Move files from Pictures folder to documents and from both to code
+for file in file_name_images:
+    if ".pdf" in file or ".txt" in file or ".csv" in file:
+        shutil.move(image_path + file, documents_path + file)
 
 #TODO Organize files from downloads folder & desktop
+
+for file in file_name_downloads:
+    if ".pdf" in file or ".txt" in file or ".csv" in file:
+        shutil.move(downloads_path + file, documents_path + file)
+    elif ".png" in file or ".jpg" in file or ".jpeg" in file or ".PNG" in file or ".JPG" in file or ".JPEG" in file:
+        shutil.move(downloads_path + file, image_path + file)
